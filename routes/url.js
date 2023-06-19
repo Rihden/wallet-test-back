463cb3aa9c0791ade36fb0b8e067f00d36417e2f
admin <nedhirboudeli@gmail.com>
2023-06-19 09:02:10 +0100

    fix route 2

--- routes/url.js
+++ routes/url.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/url");
 
-router.post("/:id", userController.redirectToUrl);
+router.get("/:id", userController.redirectToUrl);
 
 module.exports = router;
